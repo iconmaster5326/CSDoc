0ba7474cd07852f3c26c5cddd08ba461634eb61d
Iconmaster <crazybonesj@yahoo.com>
2018-05-31 18:49:17 -0400

    Added framework for tests and some basic function stubs

--- csdoc.scm
+++ csdoc.scm
@@ -1,4 +1,28 @@
 (module csdoc ()
   (import chicken scheme)
-  (print "Hello, CSDoc!")
+  (use srfi-69)
+  
+  (begin-for-syntax
+    (import chicken scheme)
+    (use srfi-69)
+    
+    (define root '())
+    (define nodes (make-hash-table #:test equal?))
+  )
+  
+  (export document)
+  (define-syntax document (er-macro-transformer (lambda (exp rename compare)
+    (let* (
+      (kind (cadr exp))
+      (rest (cddr exp))
+    )
+      (hash-table-set! nodes (car rest) (cdr rest))
+    )
+    '(void)
+  )))
+ 
+  (export docs-for)
+  (define-syntax docs-for (er-macro-transformer (lambda (exp rename compare)
+    `',(hash-table-ref/default nodes (cadr exp) #f)
+  )))
 )
---
+++ tests/run.scm
@@ -0,0 +1,4 @@
+(use csdoc)
+
+(document kind name "WOW")
+(assert (docs-for name))
